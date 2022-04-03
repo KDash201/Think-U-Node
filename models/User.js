69afb2504c261a13cbb536d26f5cc0c7054995f5
KDash201 <KVonMadison@Yahoo.com>
2022-04-02 23:05:00 -0400

    created user model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
+
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+  },
+  {
+    // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
